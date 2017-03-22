620c8fdcd179ad03a97484c1a201ccd5846a3ff9
ins <iniya96@gmail.com>
2017-03-22 17:21:12 +0530

    third commit

--- app/src/main/java/com/example/ctadmin/fabric/DonorRegistration.java
+++ app/src/main/java/com/example/ctadmin/fabric/DonorRegistration.java
@@ -2,9 +2,17 @@ package com.example.ctadmin.fabric;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
 /**
  * Created by ctadmin on 20-03-2017.
  */
 
 public class DonorRegistration extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.donor_registration);
+    }
 }
--- app/src/main/java/com/example/ctadmin/fabric/Receiver.java
+++ app/src/main/java/com/example/ctadmin/fabric/Receiver.java
@@ -9,4 +9,5 @@ import android.view.View;
  */
 
 public class Receiver extends AppCompatActivity {
+
 }
